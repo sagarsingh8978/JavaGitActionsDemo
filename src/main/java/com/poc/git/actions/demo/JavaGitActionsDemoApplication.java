08446b43dd89f109447db391f5dc3fc122a33e5a
sagarsingh8978 <sagarkumarsingh15@gmail.com>
2026-06-17 17:40:09 +0530

    "added git actions wf and some code "

---
+++ src/main/java/com/poc/git/actions/demo/JavaGitActionsDemoApplication.java
@@ -0,0 +1,13 @@
+package com.poc.git.actions.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaGitActionsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaGitActionsDemoApplication.class, args);
+	}
+
+}
